36497dad3abcc9e2c87a2b83986bb83566889585
kthxxx <152075319+kthxxx@users.noreply.github.com>
2025-02-06 12:59:25 +0800

    fix bug on input

--- GroceryDiscountCalculator/Program.cs
+++ GroceryDiscountCalculator/Program.cs
@@ -1,5 +1,6 @@
+// See https://aka.ms/new-console-template for more information
+using System;    
 using System.Collections;
-using System;
 
 namespace GroceryDiscountCalculator
 {
@@ -22,102 +23,101 @@ namespace GroceryDiscountCalculator
     }
     class Program
     {
+
         static void Main(string[] args)
         {
             const double BASE_DISCOUNT = 0.1;
-            char input;
+            string input;
+            char res = '\0';
             double totalPrice;
 
             ArrayList items = new ArrayList();
             Console.Clear();
-            try
+            do
             {
-                do
-                {
-                    Console.WriteLine("\n 1. Add a product");
-                    Console.WriteLine(" 2. Show products");
-                    Console.WriteLine(" 3. Calculate total price");
-                    Console.WriteLine(" x. Exit");
-                    Console.Write("Enter your choice: ");
+                Console.WriteLine("+-------------------------------+");
+                Console.WriteLine("Grocery Store Discount Calculator");
+                Console.WriteLine("+-------------------------------+");
+                Console.WriteLine("[1] | Add a product");
+                Console.WriteLine("[2] | Show products");
+                Console.WriteLine("[3] | Calculate total price");
+                Console.WriteLine("[x] | Exit");
+                Console.Write("Enter your choice: ");
 
-                    input = Console.ReadLine()[0];
-                    Console.Clear();
+                input = Console.ReadLine();
 
-                    switch (input)
-                    {
-                        case '1':
-                            Console.WriteLine("---------------------");
-                            Console.Write("Enter product name: ");
-                            string pname = Console.ReadLine();
+                res = input.Length > 1 ? '\0': input[0];
+                switch (res) {
+                
+                    case '1':
+                        Console.Write("Enter product name: ");
+                        string pname = Console.ReadLine();
 
-                            Console.Write("Enter price: ");
-                            double price = double.Parse(Console.ReadLine());
+                        Console.Write("Enter price: ");
+                        double price = double.Parse(Console.ReadLine());
 
-                            Console.Write("Enter quantity: ");
-                            int qtty = int.Parse(Console.ReadLine());
-                            Console.Clear();
+                        Console.Write("Enter quantity: ");
+                        int qtty = int.Parse(Console.ReadLine());
 
-                            if (items.Count > 0)
+                        if (items.Count > 0)
+                        {
+                            Boolean itemfound = false;
+
+                            foreach (Item item in items)
                             {
-                                Boolean itemfound = false;
-
-                                foreach (Item item in items)
+                                if (item.ProductName == pname)
                                 {
-                                    if (item.ProductName == pname)
-                                    {
-                                        item.Quantity += qtty;
-                                        itemfound = true;
-                                        break;
-                                    }
-                                }
-                                if (!itemfound)
-                                {
-                                    items.Add(new Item(pname, price, qtty));
+                                    item.Quantity += qtty;
+                                    itemfound = true;
+                                    break;
                                 }
                             }
-                            else
+                            if (!itemfound)
                             {
                                 items.Add(new Item(pname, price, qtty));
                             }
-                            break;
-                        case '2':
-                            Console.WriteLine("\n{0,-15} {1,-10} {2,-10}", "Product Name", "Price", "Quantity");
-                            Console.WriteLine(new string('-', 40));
-                            foreach (Item item in items)
-                            {
-                                Console.WriteLine("{0,-15} {1,-10} {2,-10}", item.ProductName, item.Price, item.Quantity);
-                            }
-                            break;
-                        case '3':
-                            totalPrice = 0;
+                        }
+                        else
+                        {
+                            items.Add(new Item(pname, price, qtty));
+                        }
+                        break;
+                    case '2':
+                        Console.WriteLine("{0,-15} {1,-10} {2,-10}", "Product Name", "Price", "Quantity");
+                        Console.WriteLine(new string('-', 40));
+                        foreach (Item item in items)
+                        {
+                            Console.WriteLine("{0,-15} {1,-10} {2,-10}", item.ProductName, item.Price, item.Quantity);
+                        }
+                        break;
+                    case '3':
+                        totalPrice = 0;
 
-                            foreach (Item item in items)
-                            {
-                                totalPrice += item.Price * item.Quantity;
-                            }
+                        foreach (Item item in items)
+                        {
+                            totalPrice += item.Price * item.Quantity;
+                        }
 
-                            if (totalPrice > 100 && totalPrice <= 200)
-                            {
-                                totalPrice -= totalPrice * BASE_DISCOUNT;
-                            }
-                            if (totalPrice > 200 && totalPrice <= 500)
-                            {
-                                totalPrice -= totalPrice * (BASE_DISCOUNT + 0.5);
-                            }
-                            if (totalPrice > 500)
-                            {
-                                totalPrice -= totalPrice * (BASE_DISCOUNT + 0.1);
-                            }
+                        if (totalPrice > 100 && totalPrice <= 200)
+                        {
+                            totalPrice -= totalPrice * BASE_DISCOUNT;
+                        }
+                        if (totalPrice > 200 && totalPrice <= 500)
+                        {
+                            totalPrice -= totalPrice * (BASE_DISCOUNT + 0.5);
+                        }
+                        if (totalPrice > 500)
+                        {
+                            totalPrice -= totalPrice * (BASE_DISCOUNT + 0.1);
+                        }
 
-                            Console.WriteLine($"Total Price: ${totalPrice}");
-                            break;
-                        default:
-                            Console.WriteLine("Invalid choice, Please try again.");
-                            break;
-                    }
-                } while (input != 'x');
-            } 
-            catch(Exception ex) { Console.WriteLine(ex.ToString()); }
+                        Console.WriteLine($"Total Price: ${totalPrice}");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            } while (res != 'x');
         }
     }
 }
